d0e51489f7ea9252ae92c3f5efb9ef81ffb9f7e9
Filip Grkinic <filip.grkinic@gmail.com>
2023-06-20 16:29:39 +0200

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mindi | Fully automated, AI powered bookmarks';
+export const SITE_DESCRIPTION = 'Mindi deeply analyzes everything you save from your browser, so you can find it later in an instant.';
+export const SITE_KEYWORDS = 'AI bookmarks, automated bookmarks, AI powered bookmarks';
+
+export const HERO_TITLE = 'Fully automated, AI powered bookmarks';
